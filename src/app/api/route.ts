272b260c3edf3e9bfa03baee2737872181e6f165
KamalGhimire577 <ghimirekrdar9840@gmail.com>
2025-10-15 12:58:57 +0545

    jhvj

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,13 +4,13 @@ import User from "@/database/models/user.schema";
 export async function GET() {
   await dbConnect(); // ✅ important
   console.log("API route hit"); // this should also appear in console
-  const newuser= await User.create({
+ await User.create({
   email:"ghimirekedar@gmail.com",
   googleId:"86687765876868",
   profileImage:"dummy image",
   username:"lakhan"
 })
   return Response.json({
-    message: "You hit the API route",newuser
+    message: "You hit the API route"
   });
 }
