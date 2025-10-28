0cdcd8bdc559b76a9150a178f0c1fbdd24deab21
KamalGhimire577 <ghimirekrdar9840@gmail.com>
2025-10-28 14:01:04 +0545

    ppp

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,14 @@
 import NextAuth from "next-auth";
-import GoogleProveder from "next-auth/providers/google"
-  const handler=NextAuth({
-providers:[
-  GoogleProveder({
-    clientId:process.env.GOOGLE_CLIENT_ID as string,
-    clientSecret:process.env.GOOGLE_CLIENT_SECREATE as string
-  }
+import GoogleProvider from "next-auth/providers/google";
 
-  )
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
-],
-secret:process.env.NEXTAUTH_SECRET
-})
-
-export{handler as GET } // mathi pani milxa 
+export { handler as GET, handler as POST };
